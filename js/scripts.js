715adb377bec69e758e1d405bdba736e91137063
Todd Walraven <cod4247@yahoo.com>
2019-12-09 14:29:41 -0800

    added name input to index and javascript to include it in results

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,7 @@
 // Front end logic below:
 $(document).ready(function() {
   $("form#survey").submit(function(event) {
+    var name = $("input#name").val();
     var question1 = parseInt($("select#question1").val());
     var question2 = parseInt($("select#question2").val());
     var question3 = parseInt($("select#question3").val());
@@ -10,15 +11,16 @@ $(document).ready(function() {
     var question5 = parseInt($("select#question5").val());
     var question6 = parseInt($("select#question6").val());
     var score = question1 + question2 + question3 + question4 + question5 + question6
-    if (score >= 7) {
-      var answer = "correct"
-    } else if (score > 1 && score <= 6) {
+    if (score >= 20) {
+      var answer = "CSS"
+    } else if (score > 16 && score <= 19) {
       var answer = "JQuery"
-    } else if (score > 2 && score <= 5) {
+    } else if (score > 12 && score <= 15) {
       var answer = "Ruby"
     } else {
       var answer = "python"
     }
+    $(".name").text(name);
     $("#answer").text(answer);
     $("#results").show();
     event.preventDefault();
