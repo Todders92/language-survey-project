b7b42f3b712288c3bddd69459f237d57f0d29675
Todd Walraven <cod4247@yahoo.com>
2019-12-09 14:59:28 -0800

    changed names of ids for questions to make javascript easier to understand

--- js/scripts.js
+++ js/scripts.js
@@ -4,20 +4,23 @@
 $(document).ready(function() {
   $("form#survey").submit(function(event) {
     var name = $("input#name").val();
-    var question1 = parseInt($("select#question1").val());
-    var question2 = parseInt($("select#question2").val());
-    var question3 = parseInt($("select#question3").val());
-    var question4 = parseInt($("select#question4").val());
-    var question5 = parseInt($("select#question5").val());
-    var question6 = parseInt($("select#question6").val());
-    var score = question1 + question2 + question3 + question4 + question5 + question6
+    var help = parseInt($("select#help").val());
+    var preference = parseInt($("select#preference").val());
+    var animals = parseInt($("select#animals").val());
+    var reading = parseInt($("select#reading").val());
+    var coding = parseInt($("select#coding").val());
+    var tv = parseInt($("select#tv").val());
+    var score = help + preference + animals + reading + coding + tv
     if (score >= 20) {
       var answer = "CSS"
     } else if (score > 16 && score <= 19) {
       var answer = "JQuery"
     } else if (score > 12 && score <= 15) {
       var answer = "Ruby"
-    } else {
+    } else if (score < 5 && score <= 11) {
+      var answer = "rust"
+    }
+    else {
       var answer = "python"
     }
     $(".name").text(name);
